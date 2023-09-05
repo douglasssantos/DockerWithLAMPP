b195af8b90e0b4f8e04c038788482a6037feed11
Douglas S. Santos <44000220+douglasssantos@users.noreply.github.com>
2023-09-05 20:50:28 -0300

    fix variable of MYSQL_MULTIPLE_DATABASES to MARIADB_MULTIPLE_DATABASES

--- docker/create_databases_mariadb.sh
+++ docker/create_databases_mariadb.sh
@@ -12,9 +12,9 @@ function create_user_and_database() {
 EOSQL
 }
 
-if [ -n "$MYSQL_MULTIPLE_DATABASES" ]; then
-	echo "Multiple database creation requested: $MYSQL_MULTIPLE_DATABASES"
-	for db in $(echo $MYSQL_MULTIPLE_DATABASES | tr ',' ' '); do
+if [ -n "$MARIADB_MULTIPLE_DATABASES" ]; then
+	echo "Multiple database creation requested: $MARIADB_MULTIPLE_DATABASES"
+	for db in $(echo $MARIADB_MULTIPLE_DATABASES | tr ',' ' '); do
 		create_user_and_database $db
 	done
 	echo "Multiple databases created"
